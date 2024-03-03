feaada6479188897e98bac56ba69664b45794d81
Jiniyas Shah <sanketshahts@gmail.com>
2024-03-03 17:20:29 +0545

    added rce & xss filter

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,30 @@ function App() {
   const [description, setDescription] = useState([]); // Initialize as an empty array
   const [cveArray, setCVEArray] = useState([]);
   const [clear, setClear] = useState(false);
+  const [rce, setRce] = useState([]);
+  const [xss, setXSS] = useState([]);
+  const [rceIsChecked, setIsChecked] = useState(false);
+  const [xssIsChecked, setXSSIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    // Toggle the state when the checkbox is checked or unchecked
+    setIsChecked(!rceIsChecked);
+
+    // Call your function or perform any other action here
+  };
+
+  const handleXSSCheckboxChange = () => {
+    setXSSIsChecked(!xssIsChecked);
+  };
+
   const handleCveIdChange = (e) => {
     if (e.target.value === "") {
       setDescription([]);
+      setRce([]);
+      setXSS([]);
       setClear(false);
+      setIsChecked(false);
+      setXSSIsChecked(false);
     }
     setCveId(e.target.value);
     const inputText = e.target.value;
@@ -26,10 +46,22 @@ function App() {
       const response = await axios.get(
         `https://cveawg.mitre.org/api/cve/${item}`
       );
-      setDescription((prev) => [
-        ...prev,
-        response.data.containers.cna.descriptions[0].value,
-      ]);
+
+      const desc = response.data.containers.cna.descriptions[0].value;
+      setDescription((prev) => [...prev, desc]);
+      if (
+        desc.toLowerCase().includes("arbitrary") ||
+        desc.toLowerCase().includes("arbitrary")
+      ) {
+        setRce((prev) => [...prev, desc]);
+      }
+
+      if (
+        desc.toLowerCase().includes("xss") ||
+        desc.toLowerCase().includes("cross-site")
+      ) {
+        setXSS((prev) => [...prev, desc]);
+      }
     } catch (error) {
       console.error("Error fetching CVE data", error);
       setDescription(["Error fetching data. Please check the CVE ID."]);
@@ -48,7 +80,11 @@ function App() {
     } else {
       setCVEArray([]);
       setCveId("");
+      setRce([]);
+      setXSS([]);
       setClear(false);
+      setIsChecked(false);
+      setXSSIsChecked(false);
     }
 
     // Perform any additional logic or actions with the cveArray
@@ -66,19 +102,72 @@ function App() {
           onChange={handleCveIdChange}
           placeholder="CVE-XXXX-XXXXX CVE-XXXX-XXXXX  CVE-XXXX-XXXXX  CVE-XXXX-XXXXX   CVE-XXXX-XXXXX  CVE-XXXX-XXXXX  CVE-XXXX-XXXXX  CVE-XXXX-XXXXX  CVE-XXXX-XXXXX  CVE-XXXX-XXXXX"
         />
+
+        <div className="checkButton">
+          <div className="innerButton">
+            <label>
+              <input
+                type="checkbox"
+                checked={rceIsChecked}
+                onChange={handleCheckboxChange}
+              />
+              RCE
+            </label>
+          </div>
+          <div className="innerButton">
+            <label>
+              <input
+                type="checkbox"
+                checked={xssIsChecked}
+                onChange={handleXSSCheckboxChange}
+              />
+              XSS
+            </label>
+          </div>
+        </div>
       </div>
       <button onClick={handleStoreArray}>
         {clear && cveId ? "Clear" : "Fetch"} Descriptions
       </button>
       <hr />
+
       <div className="result">
-        <h2>CVE Descriptions: {description.length}</h2>
-        {description.map((item, index) => (
-          <p key={index} style={{ fontSize: "20px" }}>
-            <span style={{ color: "orange" }}>{cveArray[index]}</span>
-            <span>:</span> {item}
-          </p>
-        ))}
+        {!rceIsChecked && !xssIsChecked && (
+          <>
+            <h2>CVE Descriptions: {description.length}</h2>
+            {description.map((item, index) => (
+              <p key={index} style={{ fontSize: "20px" }}>
+                <span style={{ color: "orange" }}>{cveArray[index]}</span>
+                <span>:</span> {item}
+              </p>
+            ))}
+          </>
+        )}
+        {rceIsChecked && (
+          <>
+            <h2>RCE Descriptions: {rce.length}</h2>
+
+            {rce.map((item, index) => (
+              <p key={index} style={{ fontSize: "20px" }}>
+                <span style={{ color: "orange" }}>{cveArray[index]}</span>
+                <span>:</span> {item}
+              </p>
+            ))}
+          </>
+        )}
+
+        {xssIsChecked && (
+          <>
+            <h2>XSS Descriptions: {xss.length}</h2>
+
+            {xss.map((item, index) => (
+              <p key={index} style={{ fontSize: "20px" }}>
+                <span style={{ color: "orange" }}>{cveArray[index]}</span>
+                <span>:</span> {item}
+              </p>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
